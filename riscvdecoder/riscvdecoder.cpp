7c73b08270cf05a43d42493a0a43d59c54f8ae30
viveksg <viveksgt@gmail.com>
2024-07-08 23:54:11 +0530

    decoder logic complete

--- riscvdecoder/riscvdecoder.cpp
+++ riscvdecoder/riscvdecoder.cpp
@@ -1,5 +1,5 @@
 #include "elf_parser.h"
-
+#include "isa.h"
 
 SC_MODULE(RV_DECODER)
 {
@@ -13,15 +13,49 @@ SC_MODULE(RV_DECODER)
     sc_out<sc_uint<32>> imm_12_sbtype;
     sc_out<sc_uint<32>> imm_20_ujtype;
     sc_out<sc_uint<32>> selected_imm;
-
+    sc_out<sc_uint<32>> shift_amt;
+    sc_out<sc_uint<32>> opcode_id;
     void perform_decoding()
     {
-        rs1 = instr.read().range(20, 15); // bits 15-20
-        rs2 = instr.read().range(25, 21); // bits 21-25
-        rd = instr.read().range(12, 7);   // bits 7-12
-        imm_12_itype = instr.read().range(31,20);  // bits 31,20
-        imm_12_sbtype = (instr.read().range(31,24) << 5) | instr.read().range(11,7);
-        imm_20_ujtype = instr.read().range(31,12);
+        rs1 = instr.read().range(20, 15);          // bits 15-20
+        rs2 = instr.read().range(25, 21);          // bits 21-25
+        rd = instr.read().range(12, 7);            // bits 7-12
+        imm_12_itype = instr.read().range(31, 20); // bits 31,20
+        imm_12_sbtype = (instr.read().range(31, 24) << 5) | instr.read().range(11, 7);
+        imm_20_ujtype = instr.read().range(31, 12);
+        shift_amt = instr.read().range(24, 20);
+        for (int i = 0; i < MAX_INSTR; i++)
+        {
+            uint32_t opcode_val = instr.read();
+            instr_def idef = instr_defs[i];
+            if ((opcode_val & idef.instruction_mask) == idef.instruction_match)
+            {
+                opcode_id.write(i);
+                switch (idef.immediate_type)
+                {
+                case IMM_TYPE_NONE:
+                    selected_imm = 0;
+                    break;
+                case IMM_TYPE_IMM12:
+                    selected_imm = imm_12_itype;
+                    break;
+                case IMM_TYPE_IMM20:
+                    selected_imm = imm_20_ujtype;
+                    break;
+                case IMM_TYPE_SIMM:
+                    selected_imm = imm_12_sbtype;
+                    break;
+                case IMM_TYPE_BIMM:
+                    selected_imm = (imm_12_sbtype.read() << 1);
+                    break;
+                case IMM_TYPE_JIMM20:
+                    selected_imm = (imm_20_ujtype.read() << 1);
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
     }
 
     SC_CTOR(RV_DECODER)
@@ -48,6 +82,8 @@ SC_MODULE(Testbench)
     sc_signal<sc_uint<32>> imm_12_sbtype;
     sc_signal<sc_uint<32>> imm_20_ujtype;
     sc_signal<sc_uint<32>> selected_imm;
+    sc_signal<sc_uint<32>> shift_amt;
+    sc_signal<sc_uint<32>> opcode_id;
     uint32_t mem_size = 0;
 
     void generate_clock_pulse()
@@ -70,8 +106,7 @@ SC_MODULE(Testbench)
                 pc.write(pc_val);
                 uint32_t *mem_addr = ((uint32_t *)mem);
                 instruction.write(mem_addr[pc_val]);
-                cout << "Timestamp: " << sc_time_stamp() << " pc_val " << pc_val << "| instr: " << instruction << " rs1: " << rs1 << " rs2: " << rs2 << " rd: " << rd
-                     << ", imm_12_itype: " << imm_12_itype << ", imm_12_sbtype: " << imm_12_sbtype << ", imm_20_ujtype: " << imm_20_ujtype << ", selected_imm: " << selected_imm << endl;
+                cout << "Timestamp: " << sc_time_stamp() << " pc_val " << pc_val << "| instr: " << instruction <<": "<<inst_names[opcode_id.read()]<<", reg1: "<<gpr_names[rs1.read()] <<", reg2: "<<gpr_names[rs2.read()] <<", reg_rd: "<<gpr_names[rd.read()] <<", selected_imm: " << selected_imm << endl;
                 pc_val = pc_val + 4;
             }
         }
@@ -90,6 +125,8 @@ SC_MODULE(Testbench)
         rv_dec->imm_12_sbtype(imm_12_sbtype);
         rv_dec->imm_20_ujtype(imm_20_ujtype);
         rv_dec->selected_imm(selected_imm);
+        rv_dec->shift_amt(shift_amt);
+        rv_dec->opcode_id(opcode_id);
         SC_THREAD(generate_clock_pulse);
         SC_METHOD(decode_instruction);
         sensitive << clk.posedge_event();
